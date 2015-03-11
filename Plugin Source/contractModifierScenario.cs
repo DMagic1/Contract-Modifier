43c5b8d710ef8a60b2ac354583684f36021b982a
DMagic <david.grandy@gmail.com>
2015-03-11 13:05:45 -0400

    License; reflection method for updating CW+ values

--- Plugin Source/contractModifierScenario.cs
+++ Plugin Source/contractModifierScenario.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿#region license
+/*The MIT License (MIT)
+Contract Modifier Scenario : A scenario module to store data for the addon and control save/load
+
+Copyright (c) 2015 DMagic
+
+KSP Plugin Framework by TriggerAu, 2014: http://forum.kerbalspaceprogram.com/threads/66503-KSP-Plugin-Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -358,6 +386,8 @@ namespace ContractModifier
 					updateParameterValues(p, cList[i], originals);
 				}
 			}
+			if (cmAssemblyLoad.ContractsWindowPlusParameterLoaded)
+				cmAssemblyLoad.UpdateParameterValues(p.ParamType);
 		}
 
 		private void contractChanged(float[] originals, contractTypeContainer c)
@@ -370,6 +400,8 @@ namespace ContractModifier
 					updateContractValues(c, cList[i], originals);
 				}
 			}
+			if (cmAssemblyLoad.ContractsWindowPlusContractLoaded)
+				cmAssemblyLoad.UpdateContractValues(c.ContractType);
 		}
 
 		private void updateContractValues(contractTypeContainer cC, Contract c, float[] O)
