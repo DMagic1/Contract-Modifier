ffbd0653162afc74c7f77b7ccc7b0d125a1afa95
DMagic <david.grandy@gmail.com>
2015-04-27 14:35:28 -0400

    Version bump

--- Plugin Source/Properties/AssemblyInfo.cs
+++ Plugin Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
-[assembly: AssemblyInformationalVersion("v1.3")]
-[assembly: KSPAssembly("Contract Reward Modifier", 1, 3)]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("v2.0")]
+[assembly: KSPAssembly("Contract Reward Modifier", 2, 0)]
