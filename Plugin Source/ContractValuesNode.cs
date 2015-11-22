800496207677244ff0dd108f3f724a20e6ce9400
DMagic <david.grandy@gmail.com>
2015-11-22 12:02:55 -0500

    Recover Part parameter type is allowed now

--- Plugin Source/ContractValuesNode.cs
+++ Plugin Source/ContractValuesNode.cs
@@ -276,7 +276,7 @@ namespace ContractModifier
 		{
 			foreach(Type t in parameterTypes.Values)
 			{
-				if (t.Name == "OR" || t.Name == "XOR" || t.Name == "RecoverPart" || t.Name == "AlwaysTrue" || t.Name == "Any" || t.Name == "All")
+				if (t.Name == "OR" || t.Name == "XOR" || t.Name == "AlwaysTrue" || t.Name == "Any" || t.Name == "All")
 					continue;
 				if (t.IsAbstract)
 					continue;
