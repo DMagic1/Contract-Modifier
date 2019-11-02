588c04d4be6639785e358afd9bcf4299bbb72945
DMagic1 <david.grandy@gmail.com>
2019-11-01 20:16:21 -0700

    Version

--- Plugin Source/Properties/AssemblyInfo.cs
+++ Plugin Source/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.6")]
-[assembly: AssemblyFileVersion("1.0.2.6")]
-[assembly: AssemblyInformationalVersion("v2.6")]
+[assembly: AssemblyVersion("1.0.2.7")]
+[assembly: AssemblyFileVersion("1.0.2.7")]
+[assembly: AssemblyInformationalVersion("v2.7")]
 [assembly: KSPAssembly("Contract Reward Modifier", 2, 0)]
